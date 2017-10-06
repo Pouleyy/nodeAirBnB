28131350cb145b4930bd9202272cc97bbd247321
Kevin Harismendy <kevin.harismendy@outlook.com>
2017-10-06 14:55:30 +0200

    add some user endpoint

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/**
+ * Create a user
+ */
+router.post("/", function(req, res) {
+  let userName = req.body.user;
+  let userPwd = req.body.password;
+  if(userName && userPwd) {
+    //enregistrer un user
+    //Renvoyer 200
+  }else {
+
+  }
+});
+
+
+/**
+ * Log a user
+ */
+router.get("/login", function(req, res) {
+  let userName = req.query.username;
+  let pwd = req.query.password;
+  if (userName && pwd) {
+    //check si l'user existe, si oui check si bon match, si non erreur
+  } else {
+    //si manque param erreur
+  }
+})
+
 module.exports = router;
