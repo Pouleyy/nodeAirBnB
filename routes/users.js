008fed23f003afa88f362e9cc97a4465f2959637
Kevin Harismendy <kevin.harismendy@outlook.com>
2017-10-08 00:37:25 +0200

    :sparkles: signup & login

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
+var bcrypt = require("bcrypt");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,13 +13,18 @@ router.get('/', function(req, res, next) {
  * Create a user
  */
 router.post("/", function(req, res) {
-  let userName = req.body.user;
+  let userName = req.body.username;
   let userPwd = req.body.password;
-  if(userName && userPwd) {
-    //enregistrer un user
-    //Renvoyer 200
+  let userMail = req.body.mail;
+  if(checkInfo(userName, userPwd, userMail)) {
+    User.create(userName, userPwd, userMail)
+    .then(user => {
+      sendMail(userName,userMail, "Vous êtes bien enregistré")
+      res.status(201).send(user);
+    })
+    .catch(err => res.status(500).json({error: "Username or mail might already be used"}));
   }else {
-
+    res.status(400).json({error: "Problem with the information"})
   }
 });
 
@@ -25,14 +32,33 @@ router.post("/", function(req, res) {
 /**
  * Log a user
  */
-router.get("/login", function(req, res) {
-  let userName = req.query.username;
-  let pwd = req.query.password;
-  if (userName && pwd) {
-    //check si l'user existe, si oui check si bon match, si non erreur
+router.put("/login", function(req, res) {
+  let userName = req.body.username;
+  let userPwd = req.body.password;
+  let userMail = req.body.mail;  
+  if (checkInfo(userName, userPwd, userMail)) {
+    User.exist(userName)
+    .then(user => {
+      if (user && bcrypt.compareSync(userPwd, user.password)) {
+        User.update(user._id)
+        .then(userLogged => {
+          res.status(200).json({info: "User logged"})
+        })
+        .catch(err => res.status(500).json({error: "Problem with the server"}))
+      } else {
+        res.status(400).json({error: "Problem with the information"})
+      }
+    })
+    .catch(err => res.status(500).json({error: "Problem with the server"}))
   } else {
-    //si manque param erreur
+    console.log(req.body)
+    res.status(400).json({error: "Problem with the information"})
   }
 })
+
+
+function checkInfo(userName, userPwd, userMail) {
+  return (userName && userPwd && userMail);
+}
 
 module.exports = router;
