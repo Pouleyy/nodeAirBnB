a1b5906f2b9fee1e284236a3ab873c8e9cb069d3
avondo yann <yann.avondo@gmail.com>
2017-10-20 13:32:02 +0200

    :heavy_plus_sign:

--- config/config.js
+++ config/config.js
@@ -1,9 +1,13 @@
 module.exports = function() {
 	var config = {
 		port: 9000,
-		db: "mongodb://localhost:27017/testingesup",
+		uri: 'mongodb://admin:admin@' +
+		'cluster0-shard-00-00-jy9vv.mongodb.net:27017,' +
+		'cluster0-shard-00-01-jy9vv.mongodb.net:27017,' +
+		'cluster0-shard-00-02-jy9vv.mongodb.net:27017' +
+		'/airBnb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 	};
 	// override some globals
-	process.env.PORT = config.port;
+	process.env.PORT = 9000;
 	return config;
 }();
